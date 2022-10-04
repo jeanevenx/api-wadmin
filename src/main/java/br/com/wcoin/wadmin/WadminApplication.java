be1dcc5fdb9a89265a5649117361b59c8e189ed3
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-10-03 23:20:24 -0300

    Initial commit

---
+++ src/main/java/br/com/wcoin/wadmin/WadminApplication.java
@@ -0,0 +1,13 @@
+package br.com.wcoin.wadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wcoin/wadmin/WadminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wcoin.wadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
