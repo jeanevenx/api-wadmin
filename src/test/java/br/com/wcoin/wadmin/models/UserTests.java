1938f97f03f1503f845358ff17735a0fe4efa949
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-10-04 21:49:51 -0300

    Unit testing implemented to validate user props

--- src/test/java/br/com/wcoin/wadmin/models/UserTests.java
+++ src/test/java/br/com/wcoin/wadmin/models/UserTests.java
@@ -1,3 +1,31 @@
-public class UserTest {
+package br.com.wcoin.wadmin.models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class UserTests {
+
+    @Test
+    void validateUserProps(){
+        
+        User user = new User();
+
+        user.setId(1);
+        user.setName("Jean Evenx Joseph");
+        user.setUsername("Jean");
+        user.setEmail("jeanevenx@hotmail.com");
+        user.setPassword("147@gjsm");
+        user.setAvatar("http://avatarlink.net");
+
+        assertEquals(1, user.getId());
+        assertEquals("Jean Evenx Joseph", user.getName());
+        assertEquals("Jean", user.getUsername());
+        assertEquals("jeanevenx@hotmail.com", user.getEmail());
+        assertEquals("147@gjsm", user.getPassword());
+        assertEquals("http://avatarlink.net", user.getAvatar());
+
+    }
     
 }
