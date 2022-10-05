1b346864da337f6474417f097550dae5c154d465
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-10-04 23:24:39 -0300

    Mapped the entity class "Coin"

--- src/main/java/br/com/wcoin/wadmin/models/Coin.java
+++ src/main/java/br/com/wcoin/wadmin/models/Coin.java
@@ -1,13 +1,36 @@
 package br.com.wcoin.wadmin.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name="coin")
 public class Coin {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="coin_id", nullable=false)
     private Integer id;
+
+    @Column(name="name", length=100, nullable=false)
     private String name;
+
+    @Column(name="quantity", nullable=false)
     private Double quantity;
+
+    @Column(name="symbol", length=30, nullable=false)
     private String symbol;
+
+    @Column(name="origin", length=30, nullable=true)
+    private String origin;
+
+    @Column(name="symbol", length=100, nullable=false)
     private String logo;
 }
