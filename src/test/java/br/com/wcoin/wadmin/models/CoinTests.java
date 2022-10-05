5c1265ddf6f98c483b3a09debd207d280d6ece07
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-10-04 23:05:25 -0300

    Unit testing implemented to validate coin props

---
+++ src/test/java/br/com/wcoin/wadmin/models/CoinTests.java
@@ -0,0 +1,30 @@
+package br.com.wcoin.wadmin.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CoinTests {
+
+    @Test
+    void validateCoinProps(){
+
+        Coin coin = new Coin();
+
+        coin.setId(1);
+        coin.setName("Bitcoin");
+        coin.setQuantity(158.658425);
+        coin.setSymbol("BTC");
+        coin.setLogo("http://bit.logo");
+
+        assertEquals(1, coin.getId());
+        assertEquals("Bitcoin", coin.getName());
+        assertEquals(158.658425, coin.getQuantity());
+        assertEquals("BTC", coin.getSymbol());
+        assertEquals("http://bit.logo", coin.getLogo());
+        
+    }
+    
+}
