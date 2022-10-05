8d3e39eeb968438dec508348cf5f67a0bd8cd612
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-10-04 23:32:01 -0300

    Mapped the user entity class

--- src/main/java/br/com/wcoin/wadmin/models/User.java
+++ src/main/java/br/com/wcoin/wadmin/models/User.java
@@ -1,15 +1,37 @@
 package br.com.wcoin.wadmin.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name="user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id", nullable=false)
     private Integer id;
+
+    @Column(name="name", nullable=false)
     private String name;
+
+    @Column(name="username", nullable=true, unique=true)
     private String username;
+
+    @Column(name="email", nullable=false, unique=true)
     private String email;
+
+    @Column(name="password", nullable=false)
     private String password;
+
+    @Column(name="avatar", nullable=false)
     private String avatar;
 
     
