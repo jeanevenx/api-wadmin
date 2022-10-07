6544fc970775866cbeba26ab90f52688e00122d7
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-10-07 17:50:06 -0300

    Defined some methods

--- src/main/java/br/com/wcoin/wadmin/services/ICoinService.java
+++ src/main/java/br/com/wcoin/wadmin/services/ICoinService.java
@@ -1,5 +1,11 @@
 package br.com.wcoin.wadmin.services;
 
+import br.com.wcoin.wadmin.models;
+
 public interface ICoinService {
-    
+    private Coin saveCoin(Coin data);
+    private List<Coin> getAllCoin();
+    private Coin getOneCoin(integer id);
+    private Coin updateCoin(Coin data);
+    private void deleteCoin(integer id);
 }
