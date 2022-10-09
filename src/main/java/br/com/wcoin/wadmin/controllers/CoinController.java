97851368b7033c62cf5f39deb843a317c41534c0
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-10-09 18:00:56 -0300

    Added coin controllers

--- src/main/java/br/com/wcoin/wadmin/controllers/CoinController.java
+++ src/main/java/br/com/wcoin/wadmin/controllers/CoinController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,10 +34,38 @@ public class CoinController {
 		return ResponseEntity.badRequest().build();
 	}
 	
+	@PutMapping("/coins")
+	public ResponseEntity<Coin> updateCoinData(@RequestBody Coin data){
+		Coin coin = service.updateCoin(data);
+		
+		if(coin != null) {
+			return ResponseEntity.ok(coin);
+		}
+		return ResponseEntity.badRequest().build();
+	}
+	
 	@GetMapping("/coins")
 	public List<Coin> findAllCoin(){
 		return service.getAllCoin();
 	}
+
+	@GetMapping("/coins/{id}")
+	public ResponseEntity<Coin> findCoinById(@PathVariable Integer id){
+		Coin coin = service.getOneCoin(id);
+		
+		if(coin != null) {
+			return ResponseEntity.ok(coin);
+		}
+		return ResponseEntity.status(404).build();
+		
+	}
+	
+	@DeleteMapping("/coins/{id}")
+	public void removeCoin(@PathVariable Integer id){
+			service.deleteCoin(id);
+			
+	}
+	
 	
 	
 
