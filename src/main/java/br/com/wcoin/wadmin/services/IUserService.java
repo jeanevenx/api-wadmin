11d7dedf0e1a610a4a4ec18d290310596046fe16
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-10-09 19:08:32 -0300

    Implemented user features

--- src/main/java/br/com/wcoin/wadmin/services/IUserService.java
+++ src/main/java/br/com/wcoin/wadmin/services/IUserService.java
@@ -1,5 +1,13 @@
 package br.com.wcoin.wadmin.services;
 
+import java.util.List;
+
+import br.com.wcoin.wadmin.models.User;
+
 public interface IUserService {
-    
+	public List<User> getAllUser();
+    public User getOneUser(Integer id);
+    public User updateUser(User data);
+    public void deleteUser(Integer id);
+    public User saveUser(User data);
 }
--- src/main/java/br/com/wcoin/wadmin/services/UserServiceImpl.java
+++ src/main/java/br/com/wcoin/wadmin/services/UserServiceImpl.java
@@ -1,5 +1,47 @@
 package br.com.wcoin.wadmin.services;
 
-public class UserServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.wcoin.wadmin.models.Coin;
+import br.com.wcoin.wadmin.models.User;
+import br.com.wcoin.wadmin.repositories.UserRepository;
+
+public class UserServiceImpl implements IUserService {
+	
+	@Autowired
+	private UserRepository repo;
+
+	@Override
+	public List<User> getAllUser() {
+		return (List<User>) repo.findAll();
+	}
+
+	@Override
+	public User getOneUser(Integer id) {
+		
+		return repo.findById(id).orElse(null);
+	}
+
+	@Override
+	public User updateUser(User data) {
+		if(data.getId() != null) {
+			return repo.save(data);
+		}
+		return null;
+	}
+
+	@Override
+	public void deleteUser(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User saveUser(User data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
     
 }
