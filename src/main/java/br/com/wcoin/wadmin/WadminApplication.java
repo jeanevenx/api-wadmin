c29663f1bb8795a7969193a9528e387d0a9e3089
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-10-09 15:17:54 -0300

    Swagger doc configured

--- src/main/java/br/com/wcoin/wadmin/WadminApplication.java
+++ src/main/java/br/com/wcoin/wadmin/WadminApplication.java
@@ -2,8 +2,12 @@ package br.com.wcoin.wadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@ComponentScan({"br.com.wcoin.wadmin"})
 public class WadminApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/wcoin/wadmin/config/SwaggerConfig.java
+++ src/main/java/br/com/wcoin/wadmin/config/SwaggerConfig.java
@@ -1,5 +1,40 @@
 package br.com.wcoin.wadmin.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
 public class SwaggerConfig {
 
+    @Bean
+    public Docket api(){
+        
+        return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.basePackage("br.com.wcoin.wadmin.controllers"))
+                    .build()
+                    .useDefaultResponseMessages(false)
+                    .apiInfo(apiInfo());
+
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("API da plataforma Wadmin")
+            .description("API para gerenciar investimentos em criptos")
+            .version("1.0.0")
+            .license("Apache License Version 2.0")
+            .contact(new Contact("Desenvolvido por Jean Evenx Joseph", "https://github.com/jeanevenx/api-wadmin", "jeanlovenx@gmail.com"))
+            .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+            .build();
+        
+    }
+    
 }
