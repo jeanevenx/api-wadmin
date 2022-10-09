b11c2d3945e071dcefee5bcbd372f6e0f2aeca37
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-10-09 14:57:53 -0300

    Implementing coin features

--- src/main/java/br/com/wcoin/wadmin/services/CoinServiceImpl.java
+++ src/main/java/br/com/wcoin/wadmin/services/CoinServiceImpl.java
@@ -1,5 +1,47 @@
 package br.com.wcoin.wadmin.services;
 
-public class CoinServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.wcoin.wadmin.models.Coin;
+import br.com.wcoin.wadmin.repositories.CoinRepository;
+
+@Service
+public class CoinServiceImpl implements ICoinService {
+	
+	@Autowired
+	private CoinRepository repo;
+
+	@Override
+	public List<Coin> getAllCoin() {
+		
+		return (List<Coin>) repo.findAll();
+	}
+
+	@Override
+	public Coin getOneCoin(Integer id) {
+	
+		return repo.findById(id).orElse(null);
+	}
+
+	@Override
+	public Coin updateCoin(Coin data) {
+		if(data.getId()!= null) {
+			return repo.save(data);
+		}
+		return null;
+	}
+
+	@Override
+	public void deleteCoin(Integer id) {
+		repo.deleteById(id);
+	}
+
+	@Override
+	public Coin saveCoin(Coin data) {
+		return repo.save(data);
+	}
     
 }
