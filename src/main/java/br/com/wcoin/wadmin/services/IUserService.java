f43faf3287d63603ec92bd5d24e16b85a8d155df
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-10-14 10:50:59 -0300

    Update-01

--- src/main/java/br/com/wcoin/wadmin/services/IUserService.java
+++ src/main/java/br/com/wcoin/wadmin/services/IUserService.java
@@ -5,9 +5,9 @@ import java.util.List;
 import br.com.wcoin.wadmin.models.User;
 
 public interface IUserService {
-	public List<User> getAllUser();
+	public List<User> getAllUser(); 
     public User getOneUser(Integer id);
-    public User updateUser(User data);
+    public User updateUser(User data); 
     public void deleteUser(Integer id);
-    public User saveUser(User data);
+    public User saveUser(User data); 
 }
