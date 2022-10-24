a4fbfdd3d8e0af83da11be438fed7e99c9589ca5
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-10-24 18:48:37 -0300

    Added the payload module

---
+++ src/main/java/br/com/wcoin/wadmin/payload/response/UserInfoResponse.java
@@ -0,0 +1,55 @@
+package br.com.wcoin.wadmin.payload.response;
+
+import java.util.List;
+
+public class UserInfoResponse {
+    
+    private Integer id;
+
+    private String username;
+
+    private String email;
+
+    private List<String> roles;
+
+    public UserInfoResponse(Integer id, String username, String email, List<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    
+}
