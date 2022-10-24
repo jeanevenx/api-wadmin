2373c262c62a3abcbf418e8348b7994bbbb5a22e
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2022-10-24 18:41:29 -0300

    Refactored

--- src/main/java/br/com/wcoin/wadmin/controllers/UserController.java
+++ src/main/java/br/com/wcoin/wadmin/controllers/UserController.java
@@ -7,14 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.wcoin.wadmin.models.User;
 import br.com.wcoin.wadmin.services.UserServiceImpl;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api")
@@ -23,15 +22,6 @@ public class UserController{
 	@Autowired
 	private UserServiceImpl service;
 	
-	@PostMapping("/users")
-	public ResponseEntity<User> registerUser(@RequestBody User data){
-		User newUser = service.saveUser(data);
-		
-		if(newUser !=null)
-		    return ResponseEntity.ok(newUser);
-		return ResponseEntity.badRequest().build();
-		
-	}
 	
 	@PutMapping("/users")
 	public ResponseEntity<User> updateUserData(@RequestBody User data){
